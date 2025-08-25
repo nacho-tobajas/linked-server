a83a12b19fdcc78d9ac78bd07d95b1541c5d89c2
Ignacio TR <ignacio_nob_93@hotmail.com>
2025-08-25 20:10:04 -0300

    modificando model de Users

--- src/models/usuarios/user.entity.ts
+++ src/models/usuarios/user.entity.ts
@@ -33,6 +33,9 @@ export class User {
   @Column({ name: 'birth_date', type: 'timestamp' })
   public birth_date: Date | undefined;
 
+  @Column({ name: 'creationuser', type: 'varchar' })
+  public profile_photo: string | undefined;
+
   @DeleteDateColumn({ name: 'delete_date', type: 'timestamp' })
   public delete_date: Date | undefined;
 
